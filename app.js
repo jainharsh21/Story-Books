b4da71c71d3efe3d3c6765f1e0172b6488e5b51e
HARSH JAIN <jainharsh270@gmail.com>
2020-07-27 12:31:52 +0530

    added multiple layouts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -18,12 +19,14 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Setting up handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // Routes
-
 app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
