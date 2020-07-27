4d8d65fdb088bfa01d0b387d0178ad8477c41cbb
HARSH JAIN <jainharsh270@gmail.com>
2020-07-27 11:41:56 +0530

    conencted to db

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Load config file
 
 dotenv.config({ path: "./config/config.env" });
 
+// connect to database
+
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+app.listen(
+  PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
