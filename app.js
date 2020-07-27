f2ad36d43df29308687ecce2505c4d0cd7699593
HARSH JAIN <jainharsh270@gmail.com>
2020-07-27 11:54:03 +0530

    added morgan and handlebars middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 const connectDB = require("./config/db");
 
 // Load config file
@@ -10,7 +12,16 @@ dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
+// Setting up handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 
