9c59c5903ba50fc0e249646f691066182e540907
HARSH JAIN <jainharsh270@gmail.com>
2020-07-29 12:14:56 +0530

    displaying public stories of all users

--- routes/stories.js
+++ routes/stories.js
@@ -25,4 +25,21 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show All Public Stories
+// @route   GET /stories
+
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("stories/index", { stories });
+  } catch (err) {
+    console.log(err);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
