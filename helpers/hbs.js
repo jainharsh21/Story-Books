794f8efe6c10fa666f60fa88a99412e3e1a778b5
HARSH JAIN <jainharsh270@gmail.com>
2020-07-29 19:43:09 +0530

    changed the edit button in public stories also added it to dashboard

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -20,7 +20,8 @@ module.exports = {
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
-        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fass-small"></i></a>`;
+        // return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fass-small"></i></a>`;
+        return '<a href="/stories/edit/{{_id}}" class="btn btn-float right"><i class="fas fa-edit"></i></a>';
       } else {
         return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
       }
