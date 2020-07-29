ac49417448de12c4bd2f59ea65c4f592c0a538a9
HARSH JAIN <jainharsh270@gmail.com>
2020-07-29 20:08:55 +0530

    added view each story feature

--- routes/stories.js
+++ routes/stories.js
@@ -42,6 +42,23 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    res.render("stories/show", { story });
+  } catch (err) {
+    console.log(err);
+    return res.render("error/404");
+  }
+});
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 
